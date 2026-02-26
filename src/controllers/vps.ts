36d55c7454b461129b36e6ca47206b3177f40398
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2026-02-26 11:32:34 +0530

    fix: fillTimeGaps start time consideration

--- src/controllers/vps.ts
+++ src/controllers/vps.ts
@@ -133,7 +133,10 @@ const fillTimeGaps = (data: any[], range: string, startDate: Date) => {
   // High-resolution for <= 6 hours, Hourly resolution for 12h/24h
   const isMinuteRes = ['1h', '3h', '6h'].includes(range);
 
-  if (isMinuteRes) current.setSeconds(0, 0);
+  if (isMinuteRes) {
+    current.setSeconds(0, 0);
+    current.setMinutes(current.getMinutes() + 1);
+  }
   else current.setMinutes(0, 0, 0);
 
   const end = new Date(now);
